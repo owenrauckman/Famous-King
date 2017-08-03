0e35f07eded89b109fb35dc1f922b727d2851832
Owen Rauckman <owenrauckman@gmail.com>
2017-08-03 14:31:31 -0500

    Adds Arrow Functionality To Project Page

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,10 +6,14 @@
     <h2 class="fk__heading fk__heading--dark fk__projects__heading">Check Out Our Projects</h2>
 
 
-    <div class="fk__projects__container">
+    <div class="fk__projects__container" id="js__projects__container">
       <router-link to="/projects/finding-home" class="fk__projects__project">
         <img class="fk__projects__project__poster" src="../assets/posters/findingHome.jpg"/>
         <h3 class="fk__projects__project__title">Finding Home</h3>
+      </router-link>
+      <router-link to="/projects/mustard-seed" class="fk__projects__project">
+        <img class="fk__projects__project__poster" src="../assets/posters/placeholder.jpg"/>
+        <h3 class="fk__projects__project__title">Mustard Seed</h3>
       </router-link>
       <router-link to="/projects/honduras-ministries" class="fk__projects__project">
         <img class="fk__projects__project__poster" src="../assets/posters/hondurasMinistries.jpg"/>
@@ -19,17 +23,23 @@
         <img class="fk__projects__project__poster" src="../assets/posters/flyingTreasure.jpg"/>
         <h3 class="fk__projects__project__title">Flying Treasure</h3>
       </router-link>
-      <router-link to="/projects/mustard-seed" class="fk__projects__project">
+      <router-link to="/projects/a-bright-future-for-kids" class="fk__projects__project">
         <img class="fk__projects__project__poster" src="../assets/posters/placeholder.jpg"/>
-        <h3 class="fk__projects__project__title">Mustard Seed</h3>
+        <h3 class="fk__projects__project__title">A Bright Future For Kids</h3>
       </router-link>
       <router-link to="/projects/velvet-ashes" class="fk__projects__project">
         <img class="fk__projects__project__poster" src="../assets/posters/placeholder.jpg"/>
         <h3 class="fk__projects__project__title">Velvet Ashes</h3>
       </router-link>
+      <!-- prev/next arrows -->
+      <div class="fk__projects__arrow fk__projects__arrow--left" id="js__projects__arrow__left">
+        <svg class="fk__projects__arrow__svg fk__projects__arrow__svg--left" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5"><path d="M12.664 31.332L50 68.67 87.34 31.33" fill="none" stroke="#fff" stroke-width="16.38627"/></svg>
+      </div>
+      <div class="fk__projects__arrow fk__projects__arrow--right" id="js__projects__arrow__right">
+        <svg class="fk__projects__arrow__svg fk__projects__arrow__svg--right" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="1.5"><path d="M12.664 31.332L50 68.67 87.34 31.33" fill="none" stroke="#fff" stroke-width="16.38627"/></svg>
+      </div>
+
     </div>
-
-
   </div>
 </template>
 
@@ -39,6 +49,62 @@ export default {
   data () {
     return {
       menuType: 'dark'
+    }
+  },
+  mounted(){
+    /* Global Vars */
+    const container = $('#js__projects__container');
+    const leftArrow = $('#js__projects__arrow__left');
+    const rightArrow = $('#js__projects__arrow__right');
+
+    /* Methods to call on mount */
+    this.checkWidth(container, leftArrow, rightArrow);
+
+    console.log(container.width());
+
+    /* Event Listeners */
+    container.scroll(()=>{
+      this.checkWidth(container, leftArrow, rightArrow);
+    });
+
+    $(window).resize(()=>{
+      this.checkWidth(container, leftArrow, rightArrow);
+    });
+
+    /* Click Events */
+    rightArrow.click(()=>{
+      container.animate({
+        scrollLeft: container.scrollLeft() + $('.fk__projects__project').width()
+      }, 250);
+    });
+
+    leftArrow.click(()=>{
+      container.animate({
+        scrollLeft: container.scrollLeft() - $('.fk__projects__project').width()
+      }, 250);
+    });
+
+  },
+  methods: {
+    checkWidth(container, leftArrow, rightArrow){
+      const project = $('.fk__projects__project');
+
+      if($(window).width() >= 768){
+        if(container.scrollLeft() === 0){
+          leftArrow.hide();
+        } else{
+          leftArrow.show();
+        }
+        /* add 128 to make up for 8rems of container padding*/
+        if(container.width() + container.scrollLeft() > ((project.length) * project.width() + 128) ){
+          rightArrow.hide();
+        } else{
+          rightArrow.show();
+        }
+      } else{
+        leftArrow.hide();
+        rightArrow.hide();
+      }
     }
   }
 }
@@ -143,6 +209,59 @@ export default {
         transform: scale(1.05);
         .fk__projects__project__poster{
           box-shadow: 0px 0px 30px transparentize($black, 0.8);
+        }
+      }
+    }
+    &__arrow{
+      position: fixed;
+      height: 50px;
+      width: 50px;
+      top: calc(50% + 50px);
+      bottom: 0;
+      background: white;
+      border-radius: 500px;
+      box-shadow: 0px 0px 30px transparentize($black, 0.8);
+      transition: background 0.2s ease-in-out;
+      /* Hide on everything but desktop */
+      display: none;
+      @include breakpoint('tablet'){
+        display: block;
+      }
+      @include breakpoint('desktop'){
+        display: block;
+        width: 4vw;
+        height: 4vw;
+        top: calc(50% + 4vw);
+      }
+      &:hover{
+        cursor: pointer;
+        background: darken($white, 5%);
+      }
+      &--left{
+        left: 1rem;
+      }
+      &--right{
+        right: 1rem;
+      }
+      &__svg{
+        height: 20px;
+        width: 20px;
+        position: absolute;
+        top: 50%;
+        left: 0; right: 0;
+        margin: 0 auto;
+        @include breakpoint('desktop'){
+          height: 1.5vw;
+          width: 1.5vw;
+        }
+        &--left{
+          transform: translateY(-50%) rotate(90deg);
+        }
+        &--right{
+          transform: translateY(-50%) rotate(-90deg);
+        }
+        path{
+          stroke: $brown;
         }
       }
     }
